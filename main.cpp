089b6151ffd86d593f1c6bba7b8b9f6f5d3a42d0
León G <A00573074@itesm.mx>
2022-11-06 23:49:30 +0000

    New

--- main.cpp
+++ main.cpp
@@ -4,29 +4,29 @@
 
 using namespace std;
 
-int DirX[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-int DirY[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+vector<int> DirX = { 2, 1, -1, -2, -2, -1, 1, 2 };
+vector<int> DirY = { 1, 2, 2, 1, -1, -2, -2, -1 };
 bool isPossible = false;
 
 void espacio(){
 	cout << endl;
 }
 
-bool isSafe(int i, int j, int n, vector<vector<int> >& Board) {
+bool isSafe(int i, int j, int n, vector<vector<int>>& Board) {
 	return (i >= 0 && j >= 0 && i < n && j < n && Board[i][j] == 0);
 }
 
-void knightTour(vector<vector<int>>& ChessBoard, int N, int x, int y, int visited = 1) {
+void knightTour(vector<vector<int>>& ChessBoard, int n, int x, int y, int visited = 1) {
 
 	ChessBoard[x][y] = visited;
 
-	if (visited == N * N) {
+	if (visited == n * n) {
 
 		isPossible = true;
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 
-			for (int j = 0; j < N; j++) {
+			for (int j = 0; j < n; j++) {
 				cout << ChessBoard[i][j] - 1 << " ";
 			}
 
@@ -43,8 +43,8 @@ void knightTour(vector<vector<int>>& ChessBoard, int N, int x, int y, int visite
 		int newX = x + DirX[i];
 		int newY = y + DirY[i];
 
-			if (isSafe(newX, newY, N, ChessBoard) && !ChessBoard[newX][newY]) {
-				knightTour(ChessBoard, N, newX, newY, visited + 1);
+			if (isSafe(newX, newY, n, ChessBoard) && !ChessBoard[newX][newY]) {
+				knightTour(ChessBoard, n, newX, newY, visited + 1);
 			}
 		}
 
@@ -56,26 +56,28 @@ void knightTour(vector<vector<int>>& ChessBoard, int N, int x, int y, int visite
 
 int main() {
 
+	vector<vector <int>> tablero;
 	int n;
 
 	espacio();
+
 	cout << "Input :" << endl;
 	cout << "N = ";
 	cin >> n;
 
-	vector<vector<int> > ChessBoard(n, vector<int>(n, 0));
-	int N = ChessBoard.size();
-	int X = 1;
-	int Y = 1;
+	for (int i = 0; i<n; i++){
+		vector<int> auxiliar(n,0);
+		tablero.push_back(auxiliar);
+	}
 
 	espacio();
-	
+
 	cout << "Output : " << endl;
 
-	knightTour(ChessBoard, N, X - 1, Y - 1);
+	knightTour(tablero, tablero.size(), 0, 0);
 
 	if (isPossible == false) {
-		cout << "NO HAY UNA POSIBLE SOLUCION";
+		cout << "NO HAY UNA POSIBLE SOLUCION!";
 		espacio();
 	}
 
