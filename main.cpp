b47d0c54ae4328ed1f2158bf060f843d1af5d6c3
León G <A00573074@itesm.mx>
2022-11-06 23:16:37 +0000

    New

---
+++ main.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int DirX[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int DirY[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+bool isPossible = false;
+
+bool isSafe(int i, int j, int n, vector<vector<int> >& Board) {
+	return (i >= 0 && j >= 0 && i < n && j < n && Board[i][j] == 0);
+}
+
+void knightTour(vector<vector<int>>& ChessBoard, int N, int x, int y, int visited = 1) {
+
+	ChessBoard[x][y] = visited;
+
+	if (visited == N * N) {
+
+		isPossible = true;
+
+		for (int i = 0; i < N; i++) {
+
+			for (int j = 0; j < N; j++) {
+				cout << ChessBoard[i][j] - 1 << " ";
+			}
+
+			cout << endl;
+
+		}
+
+		cout << endl;
+		return;
+
+		ChessBoard[x][y] = 0;
+		return;
+	}
+
+	for (int i = 0; i < 8; i++) {
+
+		int newX = x + DirX[i];
+		int newY = y + DirY[i];
+
+		if (isSafe(newX, newY, N, ChessBoard) && !ChessBoard[newX][newY]) {
+			knightTour(ChessBoard, N, newX, newY, visited + 1);
+		}
+	}
+
+	ChessBoard[x][y] = 0;
+}
+
+// Driver Code
+int main()
+{
+	vector<vector<int> > ChessBoard(5, vector<int>(5, 0));
+	int N = ChessBoard.size();
+	int X = 1;
+	int Y = 1;
+
+	knightTour(ChessBoard, N, X - 1, Y - 1);
+
+	// If no valid sequence of moves exist
+	if (!isPossible) {
+		cout << -1;
+	}
+
+	return 0;
+}
